163735f84aef41306eaf5cd080ee9265e1943b18
gsplendor <79778959+gsplendor@users.noreply.github.com>
2021-03-23 20:23:37 -0400

    Updated Date

--- WeatherAppGO-j.js
+++ WeatherAppGO-j.js
@@ -1,50 +1,45 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let dayIndex = date.getDate();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[dayIndex];
-    
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+function formatDate (timestamp) {
+ let date = new Date (timestamp);
+   let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
-  let monthIndex = date.getMonth();
+  let day = days[date.getDay()];
   let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
+    "January",
+    "February",
+    "March",
+    "April",
     "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
   ];
-  let month = months[monthIndex];
-  let year = date.getYear();
-  
-  return `${date}`;
-}
+  let month = months[date.getMonth()]; 
+  let year = date.getFullYear();
+  let number = date.getDate();
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+  let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;
+}
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+
+  return `Last updated on: ${day}, ${month} ${number}, ${year} at ${hours}:${minutes}`;
+}
 
 
 //Current Weather
@@ -52,12 +47,17 @@ let button = document.querySelector("#currently");
 
 function currentWeather(response) {
   document.querySelector("#city").innerHTML=response.data.name;
+  document.querySelector("#country").innerHTML = response.data.sys.country;
   document.querySelector("#degree").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
   document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-  document.querySelector("#date").innerHTML = formatDate(response.data.dt*1000);
+  
     
+let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
  let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
